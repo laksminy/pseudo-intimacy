81b17354c63815ebf1ce963f4a4124265ccaf3e5
laksminy <putu_laksminy@unud.ac.id>
2023-05-15 22:01:08 +0800

    Replace the magrittr pipe with the native one.

--- pseudo-intimacy-code.R
+++ pseudo-intimacy-code.R
@@ -5,24 +5,24 @@ library(vcd)
 
 # laks <- read_xlsx("pseudo-intimacy raw and analysed data.xlsx", sheet = "raw-1", range = "A1:Y5271")
 # 
-# laks %>% 
+# laks |> 
 #   write_tsv("data/raw-1.tsv")
 
 laks <- read_tsv("data/raw-1.tsv")
 
 # data for figure 1 =======
-df1 <- laks %>%
+df1 <- laks |>
   filter(`katagori bentuk 1` == "9. Pronomina (Pronoun)", 
          str_detect(`katagori ProN 1`, "^1(a|b)\\."),
          str_detect(`Fungsi 4`, "(^1\\. aku|^2\\. saya)"),
          `jender 1` %in% c("F", "M"))
 
 ## count =====
-df1_count <- df1 %>% 
+df1_count <- df1 |> 
   rename(gender = `jender 1`, 
          direction_of_speech = `speaker mdl dede`, 
-         pronouns = `Fungsi 4`) %>%
-  count(gender, direction_of_speech, pronouns) %>%
+         pronouns = `Fungsi 4`) |>
+  count(gender, direction_of_speech, pronouns) |>
   mutate(direction_of_speech = str_replace(direction_of_speech, "->", "→"),
          direction_of_speech = str_replace(direction_of_speech, "(host)", "\\1s"),
          pronouns = str_replace(pronouns, "^\\d\\.\\s", ""),
@@ -30,7 +30,7 @@ df1_count <- df1 %>%
                                       levels = c("younger_male_guest → older_hosts (M < H)", "older_male_guest → younger_hosts (M > H)", "younger_female_guest → older_hosts (F < H)", "older_female_guest → younger_hosts (F > H)")))
 
 ## visualisation =====
-df1_count %>% 
+df1_count |> 
   ggplot(aes(x = direction_of_speech, y = n, fill = pronouns, group = gender)) + 
   geom_col(position = "fill") + 
   coord_flip() + 
@@ -48,17 +48,17 @@ df1_count %>%
 
   
 ## data for percentage =====
-df1_count %>% 
-    group_by(direction_of_speech) %>% 
+df1_count |> 
+    group_by(direction_of_speech) |> 
     mutate(perc = n/sum(n) * 100)
   
-df1_matrix <- df1_count %>% 
-  select(-gender) %>% 
+df1_matrix <- df1_count |> 
+  select(-gender) |> 
   pivot_wider(names_from = pronouns, 
               values_from = n,
-              values_fill = 0L) %>% 
-  mutate(direction_of_speech = str_extract(direction_of_speech, "(?<=\\s)\\(. [<>] .\\)$")) %>% 
-  data.frame(row.names = 1, check.names = F) %>% 
+              values_fill = 0L) |> 
+  mutate(direction_of_speech = str_extract(direction_of_speech, "(?<=\\s)\\(. [<>] .\\)$")) |> 
+  data.frame(row.names = 1, check.names = F) |> 
   as.matrix()
 assocstats(df1_matrix)
 assoc(df1_matrix, shade = T)
@@ -71,18 +71,18 @@ chisq.test(df1_matrix)
 # X-squared = 373.58, df = 3, p-value < 2.2e-16
 
 # data for figure 2 =====
-df2 <- laks %>%
+df2 <- laks |>
   filter(`katagori bentuk 1` == "9. Pronomina (Pronoun)", 
          str_detect(`katagori ProN 1`, "^1(a|b)\\."),
          str_detect(`Fungsi 4`, "(^1\\. aku|^2\\. saya)"),
          `jender 1` %in% c("HF", "HM"))
 
 ## count =====
-df2_count <- df2 %>% 
+df2_count <- df2 |> 
   rename(gender = `jender 1`, 
          direction_of_speech = `speaker mdl dede`, 
-         pronouns = `Fungsi 4`) %>%
-  count(gender, direction_of_speech, pronouns) %>%
+         pronouns = `Fungsi 4`) |>
+  count(gender, direction_of_speech, pronouns) |>
   mutate(direction_of_speech = str_replace(direction_of_speech, "->", "→"),
          direction_of_speech = str_replace(direction_of_speech, "(guest)", "\\1s"),
          pronouns = str_replace(pronouns, "^\\d\\.\\s", ""),
@@ -90,7 +90,7 @@ df2_count <- df2 %>%
                                       levels = c("older_male_host → younger_guests (HM > G)", "younger_male_host → older_guests (HM < G)", "older_female_host → younger_guests (HF > G)", "younger_female_host → older_guests (HF < G)")))
 
 ## visualisation ====
-df2_count %>% 
+df2_count |> 
   ggplot(aes(x = direction_of_speech, y = n, fill = pronouns, group = gender)) + 
   geom_col(position = "fill") + 
   coord_flip() + 
@@ -108,14 +108,14 @@ ggsave('figs/fig2.png', width = 7.5, height = 3.5, dpi = 600)
 
 
 ## fisher exact test for fig 2 ====
-df2_matrix <- df2_count %>% 
-  select(-gender) %>% 
+df2_matrix <- df2_count |> 
+  select(-gender) |> 
   pivot_wider(names_from = pronouns, 
               values_from = n,
-              values_fill = 0L) %>% 
+              values_fill = 0L) |> 
   mutate(direction_of_speech = 
-           str_extract(direction_of_speech, "[A-Z]+ [<>] [A-Z]+")) %>% 
-  data.frame(row.names = 1, check.names = F) %>% 
+           str_extract(direction_of_speech, "[A-Z]+ [<>] [A-Z]+")) |> 
+  data.frame(row.names = 1, check.names = F) |> 
   as.matrix()
 
 chisq.test(df2_matrix)
@@ -128,17 +128,17 @@ fisher.test(df2_matrix) # not significant difference
 
 
 # data for figure 3 =====
-df3 <- laks %>%
+df3 <- laks |>
   filter(`katagori bentuk 1` == "9. Pronomina (Pronoun)", 
          str_detect(`katagori ProN 2`, "1st person"),
          str_detect(`Fungsi 4`, "(kita|kami)"),
          `jender 1` %in% c("HF", "HM", "F", "M"))
 
 ## count =====
-df3_count <- df3 %>% 
+df3_count <- df3 |> 
   rename(gender = `jender 1`, 
-         pronouns = `Fungsi 4`) %>%
-  count(gender, pronouns) %>%
+         pronouns = `Fungsi 4`) |>
+  count(gender, pronouns) |>
   mutate(pronouns = str_replace_all(pronouns, "^\\d\\.\\s", ""),
          pronouns = str_replace_all(pronouns, "^([^ ]+)(?=\\s)", "*\\1*"),
          pronouns = factor(pronouns),
@@ -149,7 +149,7 @@ df3_count <- df3 %>%
          gender = factor(gender, levels = c("Female host (HF)", "Female guests (F)", "Male host (HM)", "Male guests (M)")))
 
 ## visualisation =====
-df3_count %>% 
+df3_count |> 
   ggplot(aes(x = gender, y = n, fill = pronouns, group = gender)) + 
   geom_col(position = "fill") + 
   coord_flip() + 
@@ -165,8 +165,8 @@ df3_count %>%
 ggsave('figs/fig3.png', width = 6, height = 3.5, dpi = 600)
 
 ## data for percentage ======
-df3_count %>% 
-  group_by(gender) %>% 
+df3_count |> 
+  group_by(gender) |> 
   mutate(perc = n/sum(n))
 
 ## fisher exact test for fig 3 =====
@@ -186,30 +186,30 @@ fisher.test(df3_matrix)
 # data for figure 4 =====
 
 ## count =====
-int <- laks %>%
+int <- laks |>
   filter(`katagori bentuk 1` == "9. Pronomina (Pronoun)",
          str_detect(`katagori ProN 2`, "1st"),
-         str_detect(bentuk, "^([Aa])?ku|gue|kita")) %>%
-  count(`jender kuMkul`, name = "intimate_form (*aku*, *-ku*, *gue*, *kita*)") %>% 
-  filter(!is.na(`jender kuMkul`)) %>%
+         str_detect(bentuk, "^([Aa])?ku|gue|kita")) |>
+  count(`jender kuMkul`, name = "intimate_form (*aku*, *-ku*, *gue*, *kita*)") |> 
+  filter(!is.na(`jender kuMkul`)) |>
   rename(gender_cumulative = `jender kuMkul`)
 
-resp <- laks %>%
+resp <- laks |>
   filter(`katagori bentuk 1` == "9. Pronomina (Pronoun)",
          str_detect(`katagori ProN 2`, "1st"),
-         str_detect(bentuk, "^([Aa])?ku|gue|kita", negate = TRUE)) %>%
-  count(`jender kuMkul`, name = "respect_form (*saya*, *kami*)") %>% 
-  filter(!is.na(`jender kuMkul`)) %>%
+         str_detect(bentuk, "^([Aa])?ku|gue|kita", negate = TRUE)) |>
+  count(`jender kuMkul`, name = "respect_form (*saya*, *kami*)") |> 
+  filter(!is.na(`jender kuMkul`)) |>
   rename(gender_cumulative = `jender kuMkul`)
 
-df4 <- left_join(int, resp) %>% 
-  pivot_longer(-gender_cumulative, names_to = "pronoun_type", values_to = "n") %>% 
+df4 <- left_join(int, resp) |> 
+  pivot_longer(-gender_cumulative, names_to = "pronoun_type", values_to = "n") |> 
   mutate(gender_cumulative = str_replace(gender_cumulative, "Fk", "Female host and guests"),
          gender_cumulative = str_replace(gender_cumulative, "Mk", "Male host and guests"),
          pronoun_type = factor(pronoun_type, levels = c("respect_form (*saya*, *kami*)", "intimate_form (*aku*, *-ku*, *gue*, *kita*)")))
 
 ## visualisation =====
-df4 %>% 
+df4 |> 
   ggplot(aes(x = gender_cumulative, y = n, fill = pronoun_type)) + 
   geom_col(position = "fill") +
   coord_flip() +
@@ -221,9 +221,9 @@ ggsave('figs/fig4.png', width = 7.85, height = 3.5, dpi = 600)
 
 ## chisquare test for fig 4 =====
 
-df4_mtx <- df4 %>% 
-  pivot_wider(names_from = gender_cumulative, values_from = n) %>% 
-  data.frame(check.names = FALSE, row.names = 1) %>% 
+df4_mtx <- df4 |> 
+  pivot_wider(names_from = gender_cumulative, values_from = n) |> 
+  data.frame(check.names = FALSE, row.names = 1) |> 
   as.matrix()
 chisq.test(df4_mtx)
 # Pearson's Chi-squared test with Yates' continuity correction
@@ -238,12 +238,12 @@ chisq.test(df4_mtx)$p.value < 0.0001
 # data for figure 5 ====
 # laks <- read_xlsx("pseudo-intimacy raw and analysed data.xlsx", sheet = "raw-3", range = "A1:Y5271")
 # 
-# laks %>% 
+# laks |> 
 #   write_tsv("data/raw-2.tsv")
 
 laks <- read_tsv("data/raw-2.tsv")
 
-df5 <- laks %>%
+df5 <- laks |>
   filter(`katagori bentuk 1` == "9. Pronomina (Pronoun)", 
          str_detect(`katagori ProN 2`, "2nd peson"),
          str_detect(`Fungsi 4`, "(1\\. kamu|2\\. anda)"),
@@ -251,11 +251,11 @@ df5 <- laks %>%
          str_detect(`speaker mdl dede`, "\\([MF] [<>] H"))
 
 
-df5_count <- df5 %>% 
+df5_count <- df5 |> 
   rename(gender = `jender 1`, 
          direction_of_speech = `speaker mdl dede`, 
-         pronouns = `Fungsi 4`) %>%
-  count(gender, direction_of_speech, pronouns) %>%
+         pronouns = `Fungsi 4`) |>
+  count(gender, direction_of_speech, pronouns) |>
   mutate(direction_of_speech = str_replace(direction_of_speech, "->", "→"),
          direction_of_speech = str_replace(direction_of_speech, "(host)", "\\1s"),
          pronouns = str_replace(pronouns, "^\\d\\.\\s", ""),
@@ -268,7 +268,7 @@ df5_count <- df5 %>%
                                       levels = c("younger_male_guest → older_hosts (M < H)", "older_male_guest → younger_hosts (M > H)", "younger_female_guest → older_hosts (F < H)", "older_female_guest → younger_hosts (F > H)")))
 
 ## visualisation ====
-df5_count %>% 
+df5_count |> 
   ggplot(aes(x = direction_of_speech, y = n, fill = pronouns, group = gender)) + 
   geom_col(position = "fill") + 
   coord_flip() + 
@@ -304,20 +304,20 @@ chisq.test(df5_matrix)$residuals
 # data for figure 6 ======
 # laks <- read_xlsx("pseudo-intimacy raw and analysed data.xlsx", sheet = "raw-4", range = "A1:Y5271")
 # 
-# laks %>% write_tsv("data/raw-3.tsv")
+# laks |> write_tsv("data/raw-3.tsv")
 laks <- read_tsv("data/raw-3.tsv")
 
-df6 <- laks %>%
+df6 <- laks |>
   filter(str_detect(`katagori ProN 2`, "2nd peson"),
          `jender 1` %in% c("HF", "HM"),
          str_detect(`speaker mdl dede`, "\\(H[FM] [<>] G"))
 
 ## count =====
-df6_count <- df6 %>% 
+df6_count <- df6 |> 
   rename(gender = `jender 1`, 
          direction_of_speech = `speaker mdl dede`, 
-         pronouns = `Fungsi 4`) %>%
-  count(gender, direction_of_speech, pronouns) %>%
+         pronouns = `Fungsi 4`) |>
+  count(gender, direction_of_speech, pronouns) |>
   mutate(direction_of_speech = str_replace(direction_of_speech, "->", "→"),
          direction_of_speech = str_replace(direction_of_speech, "(guest)", "\\1s"),
          pronouns = str_replace(pronouns, "^\\d\\.\\s", ""),
@@ -330,7 +330,7 @@ df6_count <- df6 %>%
                                       levels = c("younger_female_host → older_guests (HF < G)", "older_female_host → younger_guests (HF > G)", "younger_male_host → older_guests (HM < G)", "older_male_host → younger_guests (HM > G)")))
 
 ## visualisation =====
-df6_count %>% 
+df6_count |> 
   ggplot(aes(x = direction_of_speech, y = n, fill = pronouns, group = gender)) + 
   geom_col(position = "fill") + 
   coord_flip() + 
@@ -364,27 +364,27 @@ fisher.test(df6_matrix)$p.value < 0.0001
 # data for figure 7 =======
 # laks <- read_xlsx("pseudo-intimacy raw and analysed data.xlsx", sheet = "raw-5", range = "A1:Z5249")
 # 
-# laks %>% write_tsv("data/raw-5.tsv")
+# laks |> write_tsv("data/raw-5.tsv")
 laks <- read_tsv("data/raw-5.tsv")
 
-int <- laks %>%
+int <- laks |>
   filter(`katagori bentuk 1` == "9. Pronomina (Pronoun)",
          str_detect(`katagori ProN 2`, "2nd"),
-         `Fungsi 3` %in% c("Intimacy Marker")) %>%
-  count(`jender kuMkul`, name = "intimate_form") %>% 
-  filter(!is.na(`jender kuMkul`)) %>%
+         `Fungsi 3` %in% c("Intimacy Marker")) |>
+  count(`jender kuMkul`, name = "intimate_form") |> 
+  filter(!is.na(`jender kuMkul`)) |>
   rename(gender_cumulative = `jender kuMkul`)
 
-resp <- laks %>%
+resp <- laks |>
   filter(`katagori bentuk 1` == "9. Pronomina (Pronoun)",
          str_detect(`katagori ProN 2`, "2nd"),
-         `Fungsi 3` %in% c("Respective Marker")) %>%
-  count(`jender kuMkul`, name = "respect_form") %>% 
-  filter(!is.na(`jender kuMkul`)) %>%
+         `Fungsi 3` %in% c("Respective Marker")) |>
+  count(`jender kuMkul`, name = "respect_form") |> 
+  filter(!is.na(`jender kuMkul`)) |>
   rename(gender_cumulative = `jender kuMkul`)
 
-df7 <- left_join(int, resp) %>% 
-  pivot_longer(-gender_cumulative, names_to = "pronoun_type", values_to = "n") %>% 
+df7 <- left_join(int, resp) |> 
+  pivot_longer(-gender_cumulative, names_to = "pronoun_type", values_to = "n") |> 
   mutate(gender_cumulative = str_replace(gender_cumulative, "Fk", "Female host and guests"),
          gender_cumulative = str_replace(gender_cumulative, "Mk", "Male host and guests"),
          pronoun_type = factor(pronoun_type, levels = c("intimate_form", "respect_form")))
@@ -393,7 +393,7 @@ df7 <- left_join(int, resp) %>%
 # *anda*
 
 ## visualisation =====
-df7 %>% 
+df7 |> 
   ggplot(aes(x = gender_cumulative, y = n, fill = reorder(pronoun_type, n))) + 
   geom_col(position = "fill") +
   coord_flip() +
@@ -427,13 +427,13 @@ assocstats(df7_matrix)
 # data for figure 8 =======
 # laks <- read_xlsx("pseudo-intimacy raw and analysed data.xlsx", sheet = "raw-6", range = "A1:Z5431")
 #  
-# laks %>% write_tsv("data/raw-6.tsv")
+# laks |> write_tsv("data/raw-6.tsv")
 laks <- read_tsv("data/raw-6.tsv")
 
-df8 <- laks %>% 
+df8 <- laks |> 
   filter(`jender 1` %in% c("F", "HF", "HM", "M"),
-         str_detect(`Fungsi 4`, "\\b(dia|beliau)\\b")) %>% 
-  count(`jender 1`, `Fungsi 4`) %>% 
+         str_detect(`Fungsi 4`, "\\b(dia|beliau)\\b")) |> 
+  count(`jender 1`, `Fungsi 4`) |> 
   mutate(`Fungsi 4` = str_replace_all(`Fungsi 4`, "^\\d\\.\\s+", ""),
          `jender 1` = str_replace_all(`jender 1`, "HF", "Female Host (HF)"),
          `jender 1` = str_replace_all(`jender 1`, "HM", "Male Host (HM)"),
@@ -446,12 +446,12 @@ df8 <- laks %>%
          `Fungsi 4` = str_replace_all(`Fungsi 4`, "beliau", "*beliau*"),
          `Fungsi 4` = str_replace_all(`Fungsi 4`, 
                                       "\\b(dia|ia|mereka|nya)\\b", "*\\1*"),
-         `Fungsi 4` = str_replace_all(`Fungsi 4`, "nya", "-nya")) %>% 
+         `Fungsi 4` = str_replace_all(`Fungsi 4`, "nya", "-nya")) |> 
   arrange(`jender 1`)
 df8
 
 ## visualisation ======
-df8 %>% 
+df8 |> 
   ggplot(aes(x = `jender 1`, y = n, fill = `Fungsi 4`)) + 
   geom_col(position = "fill") +
   coord_flip() +
@@ -489,13 +489,13 @@ assocstats(df8_matrix)
 # data for figure 9 =======
 # laks <- read_xlsx("pseudo-intimacy raw and analysed data.xlsx", sheet = "raw-6", range = "A1:Z5431")
 #  
-# laks %>% write_tsv("data/raw-6.tsv")
+# laks |> write_tsv("data/raw-6.tsv")
 laks <- read_tsv("data/raw-6.tsv")
 
-df9 <- laks %>% 
+df9 <- laks |> 
   filter(`jender kuMtul` %in% c("Ft", "Mt"),
-         str_detect(`Fungsi 4`, "\\b(dia|beliau)\\b")) %>% 
-  count(`jender kuMtul`, `Fungsi 4`) %>% 
+         str_detect(`Fungsi 4`, "\\b(dia|beliau)\\b")) |> 
+  count(`jender kuMtul`, `Fungsi 4`) |> 
   mutate(`Fungsi 4` = str_replace_all(`Fungsi 4`, "^\\d\\.\\s+", ""),
          `jender kuMtul` = str_replace_all(`jender kuMtul`, 
                                            "Ft", 
@@ -510,12 +510,12 @@ df9 <- laks %>%
          `Fungsi 4` = str_replace_all(`Fungsi 4`, "beliau", "*beliau*"),
          `Fungsi 4` = str_replace_all(`Fungsi 4`, 
                                       "\\b(dia|ia|mereka|nya)\\b", "*\\1*"),
-         `Fungsi 4` = str_replace_all(`Fungsi 4`, "nya", "-nya")) %>% 
+         `Fungsi 4` = str_replace_all(`Fungsi 4`, "nya", "-nya")) |> 
   arrange(`jender kuMtul`)
 df9
 
 ## visualisation ======
-df9 %>% 
+df9 |> 
   ggplot(aes(x = `jender kuMtul`, y = n, fill = `Fungsi 4`)) + 
   geom_col(position = "fill") +
   coord_flip() +
@@ -558,14 +558,14 @@ assocstats(df9_matrix)
 # data for figure 10 =======
 # laks <- read_xlsx("pseudo-intimacy raw and analysed data.xlsx", sheet = "raw-7", range = "A1:Z5435")
 # 
-# laks %>% write_tsv("data/raw-7.tsv")
+# laks |> write_tsv("data/raw-7.tsv")
 laks <- read_tsv("data/raw-7.tsv")
 
-df10 <- laks %>% 
+df10 <- laks |> 
   filter(`jender kuMtul` %in% c("Ft", "Mt"),
          str_detect(`katagori bentuk 1`, "^9\\.\\sPronomina"),
-         str_detect(`Fungsi 3`, "Marker$")) %>% 
-  count(`jender kuMtul`, `Fungsi 3`) %>% 
+         str_detect(`Fungsi 3`, "Marker$")) |> 
+  count(`jender kuMtul`, `Fungsi 3`) |> 
   mutate(`Fungsi 3` = str_replace_all(`Fungsi 3`, 
                                       "Respective", 
                                       "Respect"),
@@ -581,7 +581,7 @@ df10 <- laks %>%
 df10
 
 ## visualisation ======
-df10 %>% 
+df10 |> 
   ggplot(aes(x = `jender kuMtul`, y = n, fill = `Fungsi 3`)) + 
   geom_col(position = "fill") +
   # coord_flip() +
@@ -596,18 +596,18 @@ df10 %>%
 ggsave('figs/fig10.png', width = 7, height = 4, dpi = 300)
 
 ## Chi-square test ======
-df10 %>% 
-  pivot_wider(names_from = `Fungsi 3`, values_from = n) %>% 
-  data.frame(row.names = 1) %>% 
+df10 |> 
+  pivot_wider(names_from = `Fungsi 3`, values_from = n) |> 
+  data.frame(row.names = 1) |> 
   chisq.test()
-df10 %>% 
-  pivot_wider(names_from = `Fungsi 3`, values_from = n) %>% 
-  data.frame(row.names = 1) %>% 
-  chisq.test() %>% 
+df10 |> 
+  pivot_wider(names_from = `Fungsi 3`, values_from = n) |> 
+  data.frame(row.names = 1) |> 
+  chisq.test() |> 
   .$p.value
 ### standardised residuals ====
-df10 %>% 
-  pivot_wider(names_from = `Fungsi 3`, values_from = n) %>% 
-  data.frame(row.names = 1) %>% 
-  chisq.test() %>% 
+df10 |> 
+  pivot_wider(names_from = `Fungsi 3`, values_from = n) |> 
+  data.frame(row.names = 1) |> 
+  chisq.test() |> 
   .$stdres
